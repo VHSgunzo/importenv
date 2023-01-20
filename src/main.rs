9e2a9268c8aca9f0fa88c1e9e782cbc125483a90
VHSgunzo <wosganzo@gmail.com>
2023-01-20 21:16:31 +0300

    v0.0.4

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ use std::env;
 use chrono::Local;
 use std::io::Read;
 use std::path::Path;
+use std::ffi::CString;
 use std::process::exit;
 use std::str::from_utf8;
 use nix::unistd::execvpe;
 use std::fs::OpenOptions;
-use std::ffi::{CString, CStr};
 
 static RED: &str = "\x1b[91m";
 static RESETCOLOR: &str = "\x1b[0m";
@@ -29,7 +29,7 @@ fn main() {
     let exec_prog = CString::new(exec_args[0].clone()).unwrap();
     let exec_prog = exec_prog.as_c_str();
     let exec_args: Vec<_> = exec_args.iter()
-        .map(|s| CString::new(s.as_bytes()).unwrap()).collect();
+        .map(|arg| CString::new(arg.as_bytes()).unwrap()).collect();
     let environ = format!("/proc/{}/environ", pid);
     let environ_file_path = Path::new(&environ);
     let mut environ_file = OpenOptions::new()
@@ -41,14 +41,15 @@ fn main() {
             error_msg(&format!("{}: \"{}\"", err.to_string(), environ));
             exit(1);
     });
-    let mut env_buf = Vec::new();
-    environ_file.read_to_end(&mut env_buf).unwrap();
+    let mut environ_file_data = Vec::new();
+    environ_file.read_to_end(&mut environ_file_data).unwrap();
     let mut exec_env = Vec::new();
-    for var in env_buf.split(|b| *b == 0) {
-        exec_env.push(CString::new(from_utf8(&var).unwrap()).unwrap());
+    for var in environ_file_data.split(|b| *b == 0) {
+        let var_str = from_utf8(var).unwrap();
+        if ! var_str.is_empty() {
+            exec_env.push(CString::new(var_str).unwrap());
+        };
     };
-    exec_env.pop();
-    let exec_env: Vec<&CStr> = exec_env.iter().map(|c| c.as_c_str()).collect();
     if let Err(err) = execvpe(exec_prog, &exec_args, &exec_env) {
         error_msg(&format!("{}: {:?}", err.to_string(), exec_prog));
         exit(1);
